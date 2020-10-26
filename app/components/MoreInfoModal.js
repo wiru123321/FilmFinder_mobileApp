0477e4d8f25ef81ef0197a286fcdfad2aaa01bbf
wiru123321 <wojtekmaj2@o2.pl>
2020-10-26 11:30:32 +0100

    Fixed.

--- app/components/MoreInfoModal.js
+++ app/components/MoreInfoModal.js
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    marginTop: 22,
   },
   modalView: {
     margin: 20,
